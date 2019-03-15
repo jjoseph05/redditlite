9f726fc596e33759eff389042d96416aae561c99
Justin Joseph <justinjoseph1@gmail.com>
2019-03-15 15:27:11 -0500

    Added react, react-dom, displays title in react

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('app is running...')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Fantastic Reddit Posts and Where to Find Them';
+
+ReactDOM.render(
+  <div><h1>{title}</h1></div>,
+  document.getElementById('app')
+);
