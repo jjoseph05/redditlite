10102dc66abc24ddc0f9b61ad07c95ce67f493f6
Justin Joseph <justinjoseph1@gmail.com>
2019-03-16 01:07:44 -0500

    Added home page with popular posts

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RedditLiteApp from './components/RedditLiteApp'
+import RedditLiteApp from './components/RedditLiteApp';
 
 ReactDOM.render(
   <RedditLiteApp />,
--- src/components/RedditLiteApp.js
+++ src/components/RedditLiteApp.js
@@ -1,19 +1,48 @@
 import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 
 export default class RedditLiteApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
+      postTitles: []
     };
   }
-
+  componentDidMount() {
+    axios
+      .get('https://www.reddit.com/.json')
+      .then(res => {
+        const posts = res.data.data.children;
+        const titles = [];
+        posts.map((post)=> {
+          titles.push(post.data.subreddit_name_prefixed);
+        })
+        this.setState(() => {
+          return {
+            postTitles: this.state.postTitles.concat(titles)
+          }
+        })
+      })
+  }
   render() {
     const title = 'Fantastic Reddit Posts and Where to Find Them';
+    const { postTitles: posts } = this.state;
 
     return (
       <div>
         <h1>{title}</h1>
+        {posts.map((post, index) => {
+          return (
+            <div key={index}>
+              <a
+                href={`https://www.reddit.com/${post}`}>
+                {post}
+              </a>
+            </div>
+          )
+        })}
       </div>
     )
   }
