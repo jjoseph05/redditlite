53370134a940e0d8510b5967eea713d61595bd37
Justin Joseph <justinjoseph1@gmail.com>
2019-03-19 20:08:29 -0500

    Changed component name

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import RedditLiteApp from '../components/RedditLiteApp';
+import HomePage from '../components/HomePage';
 import NotFound from '../components/NotFound';
 import Posts from '../components/Posts';
-import Card from '../components/Card';
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Switch>
-        <Route exact path="/" component={RedditLiteApp}/>
-        <Route exact path="/r" component={Posts}/>
-        <Route path="/r/:id" component={Card}/>
+        <Route exact path="/" component={HomePage}/>
+        <Route path="/r/:id" component={Posts}/>
         <Route component={NotFound}/>
       </Switch>
     </div>
