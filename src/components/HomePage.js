ddb6d82c862e2ea5063257c88a7e4c11fdba2b9b
Justin Joseph <justinjoseph1@gmail.com>
2019-03-19 20:04:44 -0500

    Added Jest, enzyme, babel class property plugin, radium

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,35 +1,49 @@
 import React from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 
+export default class HomePage extends React.Component {
+  signal = axios.CancelToken.source();
 
-export default class RedditLiteApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      postTitles: []
+      postTitles: [],
+      loading: true
     };
   }
   componentDidMount() {
+
     axios
-      .get('https://www.reddit.com/.json')
+      .get('https://www.reddit.com/.json', { cancelToken: this.signal.token })
       .then(res => {
         const posts = res.data.data.children;
         const titles = [];
         posts.map((post)=> {
-          titles.push(post.data.subreddit_name_prefixed);
+          titles.push(post.data.subreddit);
         })
         this.setState(() => {
           return {
+            loading: false,
             postTitles: this.state.postTitles.concat(titles)
           }
         })
       })
+      .catch(error => {
+        if (axios.isCancel(error)){
+          console.log('Error: ', error.message);
+        } else {
+          console.log('error');
+          this.setState({loading: false});
+        }
+      })
+  }
+  componentWillUnmount() {
+    this.signal.cancel('Api is being cancelled');
   }
   render() {
     const title = 'Fantastic Reddit Posts and Where to Find Them';
     const { postTitles: posts } = this.state;
-
+    if (!this.state.loading) {
     return (
       <div>
         <h1>{title}</h1>
@@ -37,13 +51,19 @@ export default class RedditLiteApp extends React.Component {
           return (
             <div key={index}>
               <a
-                href={`https://www.reddit.com/${post}`}>
+                href={`/r/${post}`}>
                 {post}
               </a>
             </div>
           )
         })}
       </div>
-    )
+    )} else {
+      return (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      )
+    }
   }
 }
