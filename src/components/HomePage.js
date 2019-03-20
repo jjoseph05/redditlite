4590ac00a3aac9c6a11c7333202cc05eebbcbd19
Justin Joseph <justinjoseph1@gmail.com>
2019-03-20 17:45:52 -0500

    Added Search component to homePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Search from './Search';
 
 export default class HomePage extends React.Component {
   signal = axios.CancelToken.source();
@@ -11,32 +12,34 @@ export default class HomePage extends React.Component {
       loading: true
     };
   }
+
   componentDidMount() {
-    this.interval = setInterval(() => {
-      axios
-        .get('https://www.reddit.com/.json', { cancelToken: this.signal.token })
-        .then(res => {
-          const posts = res.data.data.children;
-          const titles = [];
-          posts.map((post)=> {
-            titles.push(post.data.subreddit);
-          })
-          this.setState(() => {
-            return {
-              loading: false,
-              postTitles: this.state.postTitles.concat(titles)
-            }
-          })
-        })
-        .catch(error => {
-          if (axios.isCancel(error)){
-            console.log('Error: ', error.message);
-          } else {
-            console.log('error');
-            this.setState({loading: false});
-          }
-        })
-    }, 2000);
+    // console.log('homepage cdMount', this.props)
+    // this.interval = setInterval(() => {
+    //   axios
+    //     .get('https://www.reddit.com/.json', { cancelToken: this.signal.token })
+    //     .then(res => {
+    //       const posts = res.data.data.children;
+    //       const titles = [];
+    //       posts.map((post)=> {
+    //         titles.push(post.data.subreddit);
+    //       })
+    //       this.setState(() => {
+    //         return {
+    //           loading: false,
+    //           postTitles: this.state.postTitles.concat(titles)
+    //         }
+    //       })
+    //     })
+    //     .catch(error => {
+    //       if (axios.isCancel(error)){
+    //         console.log('Error: ', error.message);
+    //       } else {
+    //         console.log('error');
+    //         this.setState({loading: false});
+    //       }
+    //     })
+    // }, 1000);
   }
   componentWillUnmount() {
     clearInterval(this.interval);
@@ -46,26 +49,28 @@ export default class HomePage extends React.Component {
     const title = 'Fantastic Reddit Posts and Where to Find Them';
     const { postTitles: posts } = this.state;
     if (!this.state.loading) {
-    return (
-      <div>
+      return (
+        <div>
         <h1>{title}</h1>
         {posts.map((post, index) => {
           return (
             <div key={index}>
               <a
-                href={`/r/${post}`}>
-                {post}
+                  href={`/r/${post}`}>
+                  {post}
               </a>
             </div>
           )
         })}
       </div>
-    )} else {
+      )} else {
       return (
         <div>
+          <Search history={this.props.history}/>
           <h1>Loading...</h1>
         </div>
       )
     }
   }
 }
+
