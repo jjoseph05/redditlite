da4d07db5d583fe506d0cb2a888032d185acb746
Justin Joseph <justinjoseph1@gmail.com>
2019-03-23 02:56:34 -0500

    Moved header to top level component

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header';
 import HomePage from '../components/HomePage';
 import NotFound from '../components/NotFound';
 import Posts from '../components/Posts';
@@ -7,6 +8,7 @@ import Posts from '../components/Posts';
 const AppRouter = () => (
   <BrowserRouter>
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route path="/r/:id" component={Posts}/>
