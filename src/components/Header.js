d17157f8416fc2ee4a2406528aa542a49d3ccbc4
Justin Joseph <justinjoseph1@gmail.com>
2019-03-23 02:55:04 -0500

    Extracted title into header component

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <div style={{ textAlign: "center", wordWrap: "break-word", width: 375, fontSize: "1em", margin: "0 auto" }}>
+    <h1>Fantastic Reddit Posts & Where to Find Them r/...</h1>
+  </div>
+);
+
+export default Header;
