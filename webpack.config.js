25b7a98c08ffea4be6e934f1d45fccbab3339398
Justin Joseph <justinjoseph1@gmail.com>
2019-03-22 22:35:56 -0500

    Updated webpack style loader rules

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,13 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      }, {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
       }
     ]
   },
